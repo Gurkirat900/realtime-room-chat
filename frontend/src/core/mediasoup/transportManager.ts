0d7f5a068b00f55cba7c9998cef32db15d0a272c
Gurkirat900 <gs1683676@gmail.com>
2026-03-19 01:13:32 +0530

    created transManager to create send and recv transport and manage callbacks for smooth execution of mediasoup

---
+++ frontend/src/core/mediasoup/transportManager.ts
@@ -0,0 +1,120 @@
+import * as mediasoupClient from "mediasoup-client"
+
+type OnConnectCallback = (dtlsParameters: any, direction: "send" | "recv") => void
+type OnProduceCallback = (kind: string, rtpParameters: any) => void
+
+class TransportManager {
+  private sendTransport: mediasoupClient.types.Transport | null = null
+  private recvTransport: mediasoupClient.types.Transport | null = null
+
+  /**
+   * Create Send Transport
+   */
+  createSendTransport(
+    device: mediasoupClient.Device,
+    params: any,
+    onConnect: OnConnectCallback,
+    onProduce: OnProduceCallback
+  ) {
+    if (this.sendTransport) {
+      console.log("Send transport already exists")
+      return this.sendTransport
+    }
+
+    this.sendTransport = device.createSendTransport(params)
+
+    // connect event
+    this.sendTransport.on(
+      "connect",
+      ({ dtlsParameters }, callback) => {
+        onConnect(dtlsParameters, "send")  // custom callback(tell voiceClient)
+        callback()                        // mediasoup callback(tell mediasoup "done")
+      }
+    )
+
+    // produce event
+    this.sendTransport.on(
+      "produce",
+      ({ kind, rtpParameters }, callback) => {
+        onProduce(kind, rtpParameters)
+        // IMPORTANT: callback will be called later after server responds
+      }
+    )
+
+    console.log("Send transport created")
+
+    return this.sendTransport
+  }
+
+  /**
+   * Create Recv Transport
+   */
+  createRecvTransport(
+    device: mediasoupClient.Device,
+    params: any,
+    onConnect: OnConnectCallback
+  ) {
+    if (this.recvTransport) {
+      console.log("Recv transport already exists")
+      return this.recvTransport
+    }
+
+    this.recvTransport = device.createRecvTransport(params)
+
+    this.recvTransport.on(
+      "connect",
+      ({ dtlsParameters }, callback) => {
+        onConnect(dtlsParameters, "recv")
+        callback()
+      }
+    )
+
+    console.log("Recv transport created")
+
+    return this.recvTransport
+  }
+
+  /**
+   * Get Send Transport
+   */
+  getSendTransport() {
+    if (!this.sendTransport) {
+      throw new Error("Send transport not created")
+    }
+    return this.sendTransport
+  }
+
+  /**
+   * Get Recv Transport
+   */
+  getRecvTransport() {
+    if (!this.recvTransport) {
+      throw new Error("Recv transport not created")
+    }
+    return this.recvTransport
+  }
+
+  /**
+   * Check existence
+   */
+  hasSendTransport() {
+    return !!this.sendTransport
+  }
+
+  hasRecvTransport() {
+    return !!this.recvTransport
+  }
+
+  /**
+   * Reset transports (used on leave)
+   */
+  reset() {
+    this.sendTransport?.close()
+    this.recvTransport?.close()
+
+    this.sendTransport = null
+    this.recvTransport = null
+  }
+}
+
+export const transportManager = new TransportManager()
