54e5da092fe9733309743b23dabed631028d8415
Gurkirat900 <gs1683676@gmail.com>
2026-03-24 03:02:15 +0530

    feat: implement WebSocketClient for managing WebSocket connections and event handling

---
+++ frontend/src/core/socket/WebSocketClient.ts
@@ -0,0 +1,43 @@
+type Listener = (data: any) => void
+
+export class WebSocketClient {
+  private socket: WebSocket | null = null
+  private listeners: Record<string, Listener[]> = {}  // event_type-> handler(specific fn)
+
+  connect(url: string) {
+    this.socket = new WebSocket(url)
+
+    this.socket.onopen = () => {
+      console.log("WS Connected")
+    }
+
+    this.socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data)
+      this.emit(msg.type, msg.payload)
+    }
+
+    this.socket.onclose = () => {
+      console.log("WS Disconnected")
+      // later: add reconnect logic
+    }
+  }
+
+  send(type: string, payload?: any) {
+    this.socket?.send(JSON.stringify({ type, payload }))
+  }
+
+  on(type: string, cb: Listener) {   // prevents race condition for onmessage fn
+    if (!this.listeners[type]) this.listeners[type] = []
+    this.listeners[type].push(cb)
+  }
+
+  off(type: string, cb: Listener) {
+    this.listeners[type] = this.listeners[type]?.filter(f => f !== cb)
+  }
+
+  private emit(type: string, payload?: any) {
+    this.listeners[type]?.forEach(cb => cb(payload))   // execute each fn
+  }
+}
+
+export const ws= new WebSocketClient()
